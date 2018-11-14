6cbfea67ff945421b9cd4be7de38bbf5d3c76750
Francisco Rodríguez <francico.rodriguez@prodigious.com>
2018-11-14 08:52:14 -0500

    Configuring mongo

--- src/main/java/com/ecoservices/app/Application.java
+++ src/main/java/com/ecoservices/app/Application.java
@@ -3,6 +3,8 @@ package com.ecoservices.app;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
@@ -10,8 +12,10 @@ import com.ecoservices.app.model.Role;
 import com.ecoservices.app.security.repository.RoleRepository;
 
 //@ComponentScan(basePackages = {"com.ecoservices"})
-@SpringBootApplication(scanBasePackages = { "com.ecoservices" })
+@SpringBootApplication(scanBasePackages = { "com.ecoservices" } , exclude = { MongoAutoConfiguration.class ,
+        MongoDataAutoConfiguration.class })
 @EnableMongoRepositories("com.ecoservices")
+
 public class Application {
 
     public static void main(String[] args) {
