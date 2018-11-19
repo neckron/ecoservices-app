2ec9480ed8bed296c56c7f30f310464907318131
Francisco Rodríguez <francico.rodriguez@prodigious.com>
2018-11-19 09:57:29 -0500

    Fixing home route

--- src/main/java/com/ecoservices/app/web/controller/LoginController.java
+++ src/main/java/com/ecoservices/app/web/controller/LoginController.java
@@ -1,19 +1,13 @@
 package com.ecoservices.app.web.controller;
 
-import javax.validation.Valid;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.ecoservices.app.model.User;
 import com.ecoservices.app.security.service.CustomUserDetailsService;
 
 @Controller
@@ -24,17 +18,10 @@ public class LoginController {
     @Autowired
     private CustomUserDetailsService userService;
 
-    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    @RequestMapping(value = {"/" , "/login"}, method = RequestMethod.GET)
     public ModelAndView login() {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("login");
-        return modelAndView;
-    }
-
-    @RequestMapping(value = { "/", "/home" }, method = RequestMethod.GET)
-    public ModelAndView home() {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("home");
         return modelAndView;
     }
 
