b1b94edb216efff7d45e0dd0a1017906a20112ad
Francisco Rodríguez <francico.rodriguez@prodigious.com>
2018-11-19 09:34:19 -0500

    Updating PageConfig

--- src/main/java/com/ecoservices/app/web/config/PageConfig.java
+++ src/main/java/com/ecoservices/app/web/config/PageConfig.java
@@ -19,7 +19,7 @@ public class PageConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/home").setViewName("home");
-        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/").setViewName("login");
         registry.addViewController("/login").setViewName("login");
         registry.addViewController("/signup").setViewName("signup");
         registry.addViewController("/admin/board").setViewName("/adminPages/dashboard");
